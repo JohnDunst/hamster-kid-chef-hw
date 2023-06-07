921b2201609ff45de19beaba35c29073a67854b4
JohnDunst <johnnydunst6@outlook.com>
2023-06-07 18:13:47 -0400

    person attributes

--- app.js
+++ app.js
@@ -15,3 +15,14 @@ class Hamster{
   
     }
 }
+class Person{
+  constructor(name){
+    this.name= name;
+    this.age=0;
+    this.height=0;
+    this.weight=0;
+    this.mood=0;
+    this.hampsters=[]
+    this.bankaccount=0;
+  }
+}
